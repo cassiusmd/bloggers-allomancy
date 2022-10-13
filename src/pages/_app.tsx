37cfba48cd9095dc8198aa8bb9625b35f4e412dc
Cássius <cassiusmd@gmail.com>
2022-10-13 17:52:10 -0300

    color scheme

---
+++ src/pages/_app.tsx
@@ -0,0 +1,40 @@
+import {AppProps} from 'next/app';
+import Head from 'next/head';
+import {ColorScheme, ColorSchemeProvider, MantineProvider} from '@mantine/core';
+import {useState} from "react";
+import {getCookie, setCookie} from 'cookies-next';
+import {GetServerSidePropsContext} from "next";
+import { NotificationsProvider } from '@mantine/notifications';
+
+export default function App(props: AppProps & { colorScheme: ColorScheme }) {
+    const {Component, pageProps} = props;
+
+    const [colorScheme, setColorScheme] = useState<ColorScheme>(props.colorScheme);
+
+    const toggleColorScheme = (value?: ColorScheme) => {
+        const nextColorScheme = value || (colorScheme === 'dark' ? 'light' : 'dark');
+        setColorScheme(nextColorScheme);
+        setCookie('mantine-color-scheme', nextColorScheme, {maxAge: 60 * 60 * 24 * 30});
+    };
+
+    return (
+        <>
+            <Head>
+                <title>Mantine next example</title>
+                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+                <link rel="shortcut icon" href="/favicon.svg" />
+            </Head>
+
+            <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+                <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+                    <NotificationsProvider>
+                        <Component {...pageProps} />
+                    </NotificationsProvider>
+                </MantineProvider>
+            </ColorSchemeProvider>
+        </>
+    );
+}
+App.getInitialProps = ({ctx}: { ctx: GetServerSidePropsContext }) => ({
+    colorScheme: getCookie('mantine-color-scheme', ctx) || 'light',
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { Button, Group } from "@mantine/core";
+import {ColorSchemeToggle} from "../components/ColorSchemeToggle/ColorSchemeToggle";
 
 export default function IndexPage() {
   return (
       <Group mt={50} position="center">
         <Button size="xl">Welcome to Mantine!</Button>
+          <ColorSchemeToggle />
       </Group>
   );
 }
