982b2a1126053363f63fb1fb3641e3cd939fb715
Cássius <cassiusmd@gmail.com>
2022-10-22 00:21:39 -0300

    making table responsive with block and overflow x

--- src/pages/stores/[storeid]/products/index.tsx
+++ src/pages/stores/[storeid]/products/index.tsx
@@ -2,22 +2,71 @@ import {NextPage} from 'next';
 
 import {useRouter} from 'next/router';
 import {AuthGuard} from '../../../../auth/AuthGuard';
-import {ApiGetPaginated, GetErrorsString, useFetchPaginatedApi} from '../../../../services/api/Api';
+import {useFetchPaginatedApi} from '../../../../services/api/Api';
 import {ChangeEvent, useEffect, useState} from 'react';
 import {BlogProductListing} from '../../../../models/BlogProductListing';
 import useDebounce from '../../../../hooks/useDebounce';
-import {ErrorToast} from "../../../../services/utils/Toasts";
 import {Badge, Box, Group, Loader, Pagination, Stack, Text} from "@mantine/core";
 import SearchInput from "../../../../components/Utils/SearchInput";
 import CardWithImage from "../../../../components/Cards/CardWithImage";
 
+interface ViewProductsProps {
+    storeId: string;
+    page: number;
+    pageSize: number;
+    search?: string;
+    totalPagesCallback: (totalPages: number) => void;
+}
+
+function ViewProducts({storeId, page, pageSize, search, totalPagesCallback}: ViewProductsProps) {
+
+    const {data, mutate, isLoading} = useFetchPaginatedApi<BlogProductListing>(
+        `blogger/stores/${storeId}/products`,
+        page,
+        pageSize,
+        search
+    );
+    const totalPages = data?.totalPages ?? 0;
+    const products = data?.data ?? [];
+
+    useEffect(() => {
+        totalPagesCallback(totalPages);
+    }, [totalPages]);
+
+    return (
+        <>
+            {isLoading && (<Loader/>)}
+            <Group mt={10} align={'center'}>
+                {products.map((product) => {
+                    return (
+                        <Box key={product.id}>
+                            {/*if product.created is in a week or less, add 'new' badge*/}
+
+                            <CardWithImage description={product.name} imageUuid={product.image}
+                                           badge={(Date.now() - new Date(product.created ?? Date.now()).getTime() < 14 * 24 * 3600 * 1000)
+                                               && (<Badge color="red">new</Badge>)} onClick={() => null}
+                                           sx={(theme) => ({
+                                               cursor: 'pointer',
+                                               // show border only if selected, and according to theme
+                                               minWidth: 200,
+                                               maxWidth: 200,
+
+                                           })}
+                            />
+                        </Box>
+                    );
+                })}
+            </Group>
+        </>
+    );
+}
 
 const Products: NextPage = () => {
     const router = useRouter();
     const {storeid} = router.query;
     // const [products, setProducts] = useState<BlogProductListing[]>([]);
-
-    const pageSize = 10;
+    const [totalPages, setTotalPages] = useState(0);
+    const pageSize = 21;
     const [page, setPage] = useState(1);
     const handleChange = (event: ChangeEvent<unknown>, value: number) => {
         setPage(value);
@@ -42,14 +91,7 @@ const Products: NextPage = () => {
     //         (reason) => ErrorToast(GetErrorsString(reason))
     //     );
     // }
-    const {data, mutate, isLoading} = useFetchPaginatedApi<BlogProductListing>(
-        `blogger/stores/${storeid}/products`,
-        page,
-        pageSize,
-        debouncedValue
-    );
-    const totalPages = data?.totalPages ?? 0;
-    const products = data?.data ?? [];
+
 
     // useEffect(() => {
     //     if (debouncedValue && page !== 1) {
@@ -64,32 +106,19 @@ const Products: NextPage = () => {
         <Stack spacing={5} align={'center'}>
             <Text size={'lg'}>Available products</Text>
             <SearchInput onChange={setSearch}/>
+            {storeid && (<>
+                <ViewProducts storeId={storeid.toString()} page={page} pageSize={pageSize}
+                              totalPagesCallback={setTotalPages}
+                              search={debouncedValue}/>
+                <div style={{display: 'none'}}>
+                    <ViewProducts storeId={storeid.toString()} page={page + 1} pageSize={pageSize}
+                                  totalPagesCallback={() => null}
+                                  search={debouncedValue}/>
+                </div>
+            </>)}
+            <Pagination mt={10} page={page} onChange={setPage} total={totalPages}/>
 
-            {isLoading && (<Loader/>)}
 
-            <Pagination mt={10} page={page} total={totalPages}/>
-
-            <Group mt={10} align={'center'}>
-                {products.map((product) => {
-                    return (
-                        <Box key={product.id}>
-                            {/*if product.created is in a week or less, add 'new' badge*/}
-
-                            <CardWithImage description={product.name} imageUuid={product.image}
-                                           badge={(Date.now() - new Date(product.created ?? Date.now()).getTime() < 14 * 24 * 3600 * 1000)
-                                               && (<Badge color="red">new</Badge>)} onClick={() => null}
-                                           sx={(theme) => ({
-                                               cursor: 'pointer',
-                                               // show border only if selected, and according to theme
-                                               minWidth: 200,
-                                               maxWidth: 200,
-
-                                           })}
-                            />
-                        </Box>
-                    );
-                })}
-            </Group>
         </Stack>
     );
 };
